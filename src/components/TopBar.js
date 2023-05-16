e83211f4f896c0eb70d6f41f90c79fd7cddaac9d
euchangxian <changxian2001@gmail.com>
2023-05-16 22:30:49 +0800

    Right-align Login button

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -4,19 +4,17 @@ import NavBar from "./NavBar"
 
 export default function TopBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <NavBar />
-          <Typography variant="h6" component="div" sx={{ flexGrow: 0.7 }}> 
+          <Typography variant="h6" component="div" sx={{flexGrow: 1}}> 
             ChangeX
           </Typography>
           {/* Login button */}
-          <Button variant="outlined" color="inherit" startIcon={<LoginIcon />}>
+          <Button variant="outlined" color="inherit" startIcon={<LoginIcon />} sx={{mr: "16%"}}>
             Login
           </Button>
         </Toolbar>
       </AppBar>
-    </Box>
   );
 }
