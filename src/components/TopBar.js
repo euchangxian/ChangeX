ee4456fff07d4f428e5029260f80db3df9d216da
euchangxian <changxian2001@gmail.com>
2023-05-17 00:42:51 +0800

    Made Navbar responsive

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,20 +1,20 @@
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button } from "@mui/material";
 import LoginIcon from "@mui/icons-material/Login";
 import NavBar from "./NavBar"
 
 export default function TopBar() {
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <NavBar />
-          <Typography variant="h6" component="div" sx={{flexGrow: 1}}> 
-            ChangeX
-          </Typography>
-          {/* Login button */}
-          <Button variant="outlined" color="inherit" startIcon={<LoginIcon />} sx={{mr: "16%"}}>
-            Login
-          </Button>
-        </Toolbar>
-      </AppBar>
+    <AppBar position="static">
+      <Toolbar>
+        <NavBar />
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          ChangeX
+        </Typography>
+        {/* Login button */}
+        <Button variant="outlined" color="inherit" startIcon={<LoginIcon />} sx={{ mr: "16%" }}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
