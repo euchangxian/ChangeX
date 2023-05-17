724a09a7fd46acbe9d3381267f957f2b8b651228
euchangxian <changxian2001@gmail.com>
2023-05-17 15:35:53 +0800

    Removed redundant import

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Typography, Button, IconButton, Box } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import LoginIcon from "@mui/icons-material/Login";
 
