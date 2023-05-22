b75d3bf4869c48ee5f7dc480d0480b7a1a8cabad
teojunda <teojunda@gmail.com>
2023-05-22 23:19:48 +0800

    colored budgetBar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Box, CssBaseline } from "@mui/material";
 import TopBar from "./components/TopBar";
-import NavBar from "./components/NavBar"
-import TransactionsList from "./components/TransactionsList"
-import BudgetBar from "./components/BudgetBar"
+import NavBar from "./components/NavBar";
+import TransactionsList from "./components/TransactionsList";
+import BudgetBar from "./components/BudgetBar";
 
 const drawerWidth = 240;
 
@@ -15,22 +15,37 @@ export default function App() {
   };
 
   return (
-    <>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <TopBar
-          handleDrawerToggle={handleDrawerToggle}
-          drawerWidth={drawerWidth}
-        />
-        \
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <Box
+        component="nav"
+        sx={{
+          width: { sm: drawerWidth },
+          flexShrink: { sm: 0 },
+        }}
+      >
         <NavBar
           isDrawerOpen={isDrawerOpen}
           handleDrawerToggle={handleDrawerToggle}
           drawerWidth={drawerWidth}
         />
-        <TransactionsList />
       </Box>
-      <BudgetBar />
-    </>
+      <Box sx={{ flexGrow: 1, overflow: "hidden" }}>
+        <TopBar
+          handleDrawerToggle={handleDrawerToggle}
+          drawerWidth={drawerWidth}
+        />
+        <Box
+          sx={{
+            paddingTop: (theme) => theme.spacing(8),
+            paddingBottom: (theme) => theme.spacing(2),
+            overflowY: "auto",
+          }}
+        >
+          <BudgetBar />
+          <TransactionsList />
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- client/src/components/BudgetBar.js
+++ client/src/components/BudgetBar.js
@@ -6,13 +6,39 @@ import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 
 export default function BudgetBar() {
+  const expenditure = 140;
+  const budget = 150;
+  const budgetPct = expenditure * 100 / budget;
+  const datePct = new Date().getDate() / 30;
+  const onTrackPct = budgetPct / datePct;
+
+  let progressBarColor = "primary";
+  if (onTrackPct > 100) {
+    progressBarColor = "error";
+  }
+
   return (
-    <Box sx={{ display: "flex", alignItems: "center" }}>
-      <Box sx={{ width: "100%", mr: 1 }}>
-        <LinearProgress variant="determinate" value="50" />
+    <Box sx={{ margin: "16px" }}>
+      <Box display="flex" flexDirection="row" alignItems="center" mb={2}>
+        <Box flexGrow={1}>
+          <Typography variant="h4">Your budget for January</Typography>
+        </Box>
+        <Box>
+          <Typography variant="h4" component="span">
+            ${expenditure}
+          </Typography>
+          <Typography variant="body1" component="span" gutterBottom>
+            /${budget}
+          </Typography>
+        </Box>
       </Box>
-      <Box sx={{ minWidth: 35 }}>
-        <Typography variant="body2" color="text.secondary">50%</Typography>
+      <Box flexGrow={1}>
+        <LinearProgress
+          variant="determinate"
+          value={budgetPct}
+          sx={{ height: 10 }}
+          color={ progressBarColor }
+        />
       </Box>
     </Box>
   );
