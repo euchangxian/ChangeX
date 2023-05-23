42ad1507ddb1525d43527747d5ebfc39ef6432b0
euchangxian <changxian2001@gmail.com>
2023-05-23 22:42:20 +0800

    Added Sign In/Up functionality

--- server/index.mjs
+++ server/index.mjs
@@ -1,22 +1,47 @@
 import express from "express";
+import session from "express-session";
 import bodyParser from "body-parser";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import "express-async-errors";
+import authRouter from "./routes/auth.mjs";
+import MongoStore from "connect-mongo";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+    optionsSuccessStatus: 200
+  })
+);
 
 // TODO: Load routes
+// app.post("/signin", (req, res) => {
+//   console.log(req.body);
+//   res.sendStatus(201);
+// });
+// app.post("/signup", (req, res) => {
+//   console.log(req.body);
+//   res.sendStatus(201);
+// });
+
+app.use("/", authRouter);
+
 
 // Global error handling
 app.use((err, _req, res, next) => {
   res.status(500).send("Uh oh! An unexpected error occurred.");
 })
+
+app.get("/", (req, res) => {
+  console.log("Hello \"/\"");
+  res.status(201).send("User logged in successfully");
+});
 
 // Start the Express server
 app.listen(PORT, () => {
--- server/routes/auth.mjs
+++ server/routes/auth.mjs
@@ -1,41 +1,123 @@
+import "../loadEnvironment.mjs";
 import express from "express";
 import session from "express-session";
 import passport from "passport";
 import LocalStrategy from "passport-local";
 import bcrypt from "bcrypt";
 import db from "../db/conn.mjs";
+import MongoStore from "connect-mongo";
 
 const router = express.Router();
+const users = db.collection("users");
 
-// Configure the local strategy.
-passport.use(
-  new LocalStrategy((email, password, done) => {
-    try {
-      // Find the user in the users Collection
-      const user = db.users.findOne({ email: email });
-
-      // If user not found, return error
-      if (!user) {
-        return done(null, false, { message: "Incorrect email." });
-      }
-
-      // Compare the password using bcrypt
-      const isMatch = bcrypt.compare(password, user.password);
-
-      // If password is not a match, return error
-      if (!isMatch) {
-        return done(null, false, { message: "Incorrect password." });
-      }
-
-      // If authentication succeeds, return the user
-      return done(null, user);
-    } catch (err) {
-      return done(err);
-    }
+router.use(
+  session({
+    secret: process.env.SECRET,
+    saveUninitialized: false, // don't create session until something stored
+    resave: false, //don't save session if unmodified
+    store: MongoStore.create({
+      mongoUrl: process.env.ATLAS_URI,
+      touchAfter: 24 * 3600 // time period in seconds
+    })
   })
 );
 
-router.post("/signin/password", passport.authenticate("local", {
+// Passport configuration for sign in
+passport.use(
+  "signIn",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password"
+    },
+    async (username, password, done) => {
+      // Find the user in the users Collection
+      users.findOne({ username: username }).then(existingUser => {
+        // If user not found, return error
+        if (!existingUser) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        // Compare the password using bcrypt
+        bcrypt.compare(password, existingUser.password, (err, isMatch) => {
+          // If password is not a match, return error
+          if (!isMatch) {
+            console.log("Incorrect password");
+            return done(null, false, { message: "Incorrect password." });
+          }
+          // If password is a match, return the existing user.
+          console.log(`User ${existingUser.username} signed in successfully`);
+          return done(null, existingUser);
+        });
+      }).catch(error => {
+        console.log(error);
+        return done(err);
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  process.nextTick(() => {
+    done(null, { id: user._id, username: user.username });
+  })
+});
+
+passport.deserializeUser((user, done) => {
+  process.nextTick(() => {
+    done(null, user);
+  })
+});
+
+const saltRounds = 10;
+// Passport configuration for sign ups
+passport.use(
+  "signUp",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+      passReqToCallback: true // allows us to pass back the entire request to the callback
+    },
+    async (req, username, password, done) => {
+      // Check if user already exists with the form's username
+      users.findOne({ username: username }).then(existingUser => {
+        if (existingUser) {
+          return done(null, false, { message: "Username is already taken!" });
+        }
+      });
+      // If there is no existing users with that username, hash password and insert into the users collection
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+          console.log(err);
+          return done(err);
+        }
+        const newUser = {
+          username: username,
+          password: hash
+        };
+        users.insertOne(newUser).then(result => {
+          console.log(result);
+          users.findOne(newUser).then(user => {
+            console.log(`User ${user.username} successfully inserted into users collection`);
+          });
+          return done(null, result);
+        }).catch(err => {
+          console.log(err);
+          return done(err);
+        });
+      })
+    }
+  )
+);
+
+router.post("/signin/password", passport.authenticate("signIn", {
   successRedirect: "/",
-  failureRedirect: "/signin"
+  failureRedirect: "/"
 }));
+
+router.post("/signup", passport.authenticate("signUp", {
+  successRedirect: "/",
+  failureRedirect: "/"
+}));
+
+export default router;
