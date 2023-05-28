b8cafdc69469c97724760a97d8a8727535564ef0
euchangxian <changxian2001@gmail.com>
2023-05-28 21:08:51 +0800

    Added documentation for authRoutes

--- server/index.mjs
+++ server/index.mjs
@@ -8,6 +8,7 @@ import MongoStore from "connect-mongo";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.mjs";
+import authJwt from "./middlewares/authJwt.mjs";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
@@ -35,6 +36,9 @@ app.use(
 );
 
 // TODO: Load routes
+// status 200 is sent for successful login/signup. 
+// status 400 is sent for incorrect username/password.
+// status 500 is sent for errors.
 app.use(authRoutes);
 
 app.get("/", (req, res) => {
