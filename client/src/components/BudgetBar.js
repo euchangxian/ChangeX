278cb524a69e1e7c01422dfdf2b5d683ca057382
teojunda <teojunda@gmail.com>
2023-06-02 14:21:54 +0800

    budget bar faulty test

--- client/src/components/BudgetBar.js
+++ client/src/components/BudgetBar.js
@@ -4,16 +4,39 @@ import LinearProgress, {
 } from "@mui/material/LinearProgress";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import axios from "../apis/axios";
+import { toast } from "react-toastify";
+import dayjs from "dayjs";
 
 export default function BudgetBar() {
   const [userMonthlyBudget, setUserMonthlyBudget] = React.useState({
     user_id: 1,
     budget: 1500,
-    startDate:"Jan",
+    startDate: "Jan",
   });
-  const expenditure = 140;
+
+  const [spending, setSpending] = React.useState(0);
+
+  const fetchSpendingByMonthYear = async () => {
+    const date = dayjs();
+    await axios
+      .get(`/getspending/${date}`)
+      .then((result) => {
+        if (result.status == 200) {
+          setSpending(result.data);
+        }
+      })
+      .catch((error) => {
+        toast.error("Failed to get Spending!");
+      });
+  };
+
+  React.useEffect(() => {
+    fetchSpendingByMonthYear();
+  }, []);
+
   const budget = 150;
-  const budgetPct = (expenditure * 100) / budget;
+  const budgetPct = (spending * 100) / budget;
   const datePct = new Date().getDate() / 30;
   const onTrackPct = budgetPct / datePct;
 
@@ -26,11 +49,13 @@ export default function BudgetBar() {
     <Box sx={{ margin: "16px" }}>
       <Box display="flex" flexDirection="row" alignItems="center" mb={2}>
         <Box flexGrow={1}>
-          <Typography variant="h4">Your budget for { userMonthlyBudget.startDate }</Typography>
+          <Typography variant="h4">
+            Your budget for {userMonthlyBudget.startDate}
+          </Typography>
         </Box>
         <Box>
           <Typography variant="h4" component="span">
-            ${expenditure}
+            ${spending}
           </Typography>
           <Typography variant="body1" component="span" gutterBottom>
             /${budget}
